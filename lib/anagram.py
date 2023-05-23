46a8f7bdc6ea11bea91a6820ce1702d7ab514ee7
Historia Montague <historia.montague@outlook.com>
2023-05-23 11:44:43 -0600

    Finished Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        return[n for n in words if sorted(list(n)) == sorted(list(self.word))]
